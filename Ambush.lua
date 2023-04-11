cc466e8b93934a95b9f9a00d808e7ae5c7612709
CloneNikita <120546590+CloneNikita@users.noreply.github.com>
2023-04-11 17:29:15 +0500

    Update Ambush.lua

--- Ambush.lua
+++ Ambush.lua
@@ -1,4 +1,4 @@
-
+game.Workspace.Ambience_Ambush:Play()
 local Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
 
 -- Create entity
