cb28a7419558050ee7d8c5efedcd3f076d4c03d8
CloneNikita <120546590+CloneNikita@users.noreply.github.com>
2023-04-13 19:05:59 +0500

    Update Trolling.lua

--- Trolling.lua
+++ Trolling.lua
@@ -1,12 +1,3 @@
-function notif(title,desc,rea)
-    local a=loadstring(game:HttpGet("https://raw.githubusercontent.com/SoriSoft/HazeServices/main/CustomService/Achievements/Service.lua"))()
-    a.Get({
-        Title = title,
-        Desc = desc,
-        Reason = rea,
-        Image = "https://cdn.britannica.com/43/172743-138-545C299D/overview-Barack-Obama.jpg?w=800&h=450&c=crop"
-    })
-end
 notif("Script working", "wow trolled by devs","LMAO")
 game.Workspace.CurrentRooms.ChildAdded:Connect(function()
 if game.Workspace.Ambience_Seek.Playing == false then
