e28bad7b64157af74d8d52a7bd1c34f53c3db770
CloneNikita <120546590+CloneNikita@users.noreply.github.com>
2023-04-13 19:37:10 +0500

    Update Trolling.lua

--- Trolling.lua
+++ Trolling.lua
@@ -1,8 +1,4 @@
-function notif(title,desc,rea)
-    local a=loadstring(game:HttpGet("https://raw.githubusercontent.com/SoriSoft/HazeServices/main/CustomService/Achievements/Service.lua"))()
-    a.Get({
-        Title = title,
-        Desc = desc,
+
         Reason = rea,
         Image = "https://cdn.britannica.com/43/172743-138-545C299D/overview-Barack-Obama.jpg?w=800&h=450&c=crop"
     })
